2ce59286cf732c78408dcc7aafa7133d0847d4d7
Havok <rngdhavok@hotmail.com>
2022-12-19 18:31:00 -0500

    added temporary code to work with

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,44 @@
 // Assignment code here
 const chars = {
-    alpha: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    alphaUpper: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    alphaLower: "abcdefghijklmnopqrstuvwxyz",
     numbers: "0123456789",
     symbols: " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+}
+
+const passwordLength = function () {
+  // prompting window for length of password
+  const length = window.prompt("How long do you want your password to be?");
+  // tryAgain function
+  function tryAgain() {
+    // Get tryAgain boolean with prompt
+    const tryAgain = window.confirm("That's not a number btween 8 and 128!");
+    // Checking their response on "Try again? question"
+    if (tryAgain) {
+      return passwordLength();
+    }
+  }
+
+  // Checking if you put a valid password length
+  if (length > 8  && length < 128) {
+    // alerting window if so
+    window.alert("You are the one!");
+    // Otherwise you might be the one
+  } else if (name.toUpperCase() === "TRINITY") {
+    // asking for confirmation on Oneness status
+    const isTheOne = window.confirm("Whoa, wait, do you think maybe you're the one?");
+    // Checking if response is confirm
+    if (isTheOne) {
+      // and alerting window if so
+      window.alert("Who needs Neo!!?\n\nYou are the one!!!");
+      // if they deny Oneness, ask to try agin
+    } else {
+      tryAgain();
+    }
+    // If they aren't Neo or Trinity, ask to try again
+  } else {
+    tryAgain();
+  }
 }
 
 
