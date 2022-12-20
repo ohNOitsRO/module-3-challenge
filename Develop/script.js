3263548ddf0dc78ad53489bd247a47aa53d786f8
Delgado <rxd718@miami.edu>
2022-12-20 15:32:38 -0500

    Fixed main if loop

--- Develop/script.js
+++ Develop/script.js
@@ -6,65 +6,63 @@ const chars = {
     symbols: " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"   
 }
 
+let emptyPassword = [];
+
 const passwordLength = function () {
-  // prompting window for length of password
-  const length = window.prompt("How long do you want your password to be?");
-  // tryAgain function
-  function tryAgain() {
-    // Get tryAgain boolean with prompt
-    const tryAgain = window.confirm("That's not a number between 8 and 128!");
-    // Checking their response on "Try again? question"
-    if (tryAgain) {
-      return passwordLength();
-    }
-    else {
-      window.alert("Sad to see you go! Come back soon!")
-    }
-  }
 
-
-  if (length > 8  && length < 128) {
-    
-   const ifUpper = window.prompt("Excellent! Would you like Upper Case characters in your password?");
-    
-  } else if (ifUpper === false) {
-    
-    const isTheOne = window.confirm("Whoa, wait, do you think maybe you're the one?");
+  const length = window.prompt("How long do you want your password to be?"); 
   
-  } else {
+    if (length >= 8  && length <= 128) {
 
-
-  }
-
-    if (isTheOne) {
+      window.confirm("Excellent! Would you like Upper Case characters in your password?");       
+        if (true) {          
+          emptyPassword.push(Math.floor(Math.random * chars.alphaUpper[length]));
+        }
       
-      window.alert("Who needs Neo!!?\n\nYou are the one!!!");
-   
-    } else {
-      tryAgain();
+      window.confirm("Super! Would you like Lower Case characters in your password?");
+        if (true) {
+          emptyPassword.push(Math.floor(Math.random * chars.alphaLower[length]));
+        }
+
+      window.confirm("Awesome! Would you like Numbers in your password?");
+        if (true) {
+          emptyPassword.push(Math.floor(Math.random * chars.numbers[length]));
+        }
+
+      window.confirm("Fantastic! Would you like Symbols in your password?");
+        if (true) {
+          emptyPassword.push(Math.floor(Math.random * chars.alphaLower[length]));
+        }
+
     }
- 
-  } else {
-    tryAgain();
-  }
-}
+    
+    else {
+
+        window.alert("This isn't a number between 8 and 128! Try Again!");
+        passwordLength();
+      
+      }
+
+    }
 
 passwordLength();
 
+console.log(emptyPassword);
+    
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
+// // Get references to the #generate element
+// var generateBtn = document.querySelector("#generate");
 
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// // Write password to the #password input
+// function writePassword() {
 
-  passwordText.value = password;
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
-}
+//   passwordText.value = password;
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// }
 
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
